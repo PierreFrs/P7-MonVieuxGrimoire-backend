87a34a88d19f3375c352817651288da1f4382d20
Pierre <pierrefraisse90@gmail.com>
2023-07-17 19:23:50 +0200

    auth middleware

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
+
 const bookCtrl = require("../controllers/books");
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getOneBook);
 router.get("/bestrating", bookCtrl.getBestRatedBooks);
-router.post("/", bookCtrl.createBook);
-router.put("/:id", bookCtrl.modifyBook);
-router.delete("/:id", bookCtrl.deleteBook);
-router.post("/:id/rating", bookCtrl.rateBook);
+router.post("/", auth, bookCtrl.createBook);
+router.put("/:id", auth, bookCtrl.modifyBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
+router.post("/:id/rating", auth, bookCtrl.rateBook);
 
 module.exports = router;
