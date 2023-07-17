e952703520b331dee2475fbad9dbdafe37035cdd
Pierre <pierrefraisse90@gmail.com>
2023-07-17 16:33:11 +0200

    routes files

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 const uri = `mongodb+srv://pierrefraisse90:${process.env.PASSWORD}@booksdb.bnllrtl.mongodb.net/?retryWrites=true&w=majority`;
 const mongoose = require("mongoose");
 
-const Book = require("./models/Book");
-const User = require("./models/User");
+const booksRoutes = require("./routes/books");
+const usersRoutes = require("./routes/users");
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,86 +28,9 @@ app.use((req, res, next) => {
 });
 
 // Users requests
-
-// User signup
-app.post("api/auth/signup", (req, res, next) => {
-  const user = new User({
-    ...req.body,
-  });
-  user
-    .save()
-    .then(() => res.status(201).json({ message: "Utilisateur enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// User login
-app.post("api/auth/login", (req, res, next) => {
-  const user = new User({
-    ...req.body,
-  });
-  user
-    .save()
-    .then(() => res.status(201).json({ message: "Utilisateur connecté !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/auth", usersRoutes);
 
 // Books requests
-
-// GET all books
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// GET one book
-app.get("/api/books/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-// GET best rated books
-app.get("/api/books/bestrating", (req, res, next) => {
-  Book.find()
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-// POST one book
-app.post("/api/books", (req, res, next) => {
-  const book = new Book({
-    ...req.body,
-  });
-  book
-    .save()
-    .then(() => res.status(201).json({ message: "Livre enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// PUT (update) one book
-app.put("/api/books/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Livre modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// DELETE one book
-app.delete("/api/books/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Livre supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// POST book rating
-app.post("/api/books/:id/rating", (req, res, next) => {
-  const book = new Book({
-    ...req.body,
-  });
-  book
-    .save()
-    .then(() => res.status(201).json({ message: "Livre enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/books", booksRoutes);
 
 module.exports = app;
