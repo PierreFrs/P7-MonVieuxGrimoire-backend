b2ba6222931d9d2a99cfd12ef179a30ba275c35e
Pierre <pierrefraisse90@gmail.com>
2023-07-17 14:27:59 +0200

    rounting : 1st route

--- app.js
+++ app.js
@@ -2,23 +2,42 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Requête reçue");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue" });
-  next();
-});
-
-app.use((req, res) => {
-  console.log("Réponse envoyée avec succès !");
+app.use("/api/books", (req, res, next) => {
+  const books = [
+    {
+      userId: "oeihfzeoi",
+      title: "Mon premier livre",
+      author: "Toto",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      year: 2022,
+      genre: "Roman",
+      ratings: [
+        {
+          userId: "user1",
+          grade: 5,
+        },
+      ],
+      averageRating: 4.2,
+    },
+    {
+      userId: "hifhbzizs",
+      title: "Mon second livre",
+      author: "Tata",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      year: 2022,
+      genre: "Roman",
+      ratings: [
+        {
+          userId: "user2",
+          grade: 5,
+        },
+      ],
+      averageRating: 4.2,
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
