47a6af00cc6ee35446def6f9de431d5c0cc27af7
Pierre <pierrefraisse90@gmail.com>
2023-07-18 11:56:22 +0200

    bug : next in auth.js

--- models/Book.js
+++ models/Book.js
@@ -9,8 +9,8 @@ const bookSchema = mongoose.Schema({
   genre: { type: String, required: true },
   ratings: [
     {
-      userId: { type: String, required: true },
-      grade: { type: Number, required: true },
+      userId: { type: String, required: false },
+      grade: { type: Number, required: false },
     },
   ],
   averageRating: { type: Number, required: false },
